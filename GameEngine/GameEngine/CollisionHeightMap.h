9e5aaff1ef85afe29fab9242ab150a28dbe1e3d7
Arran <arran_ford@live.com>
2013-04-10 11:01:31 +0800

    Comments

--- GameEngine/GameEngine/CollisionHeightMap.h
+++ GameEngine/GameEngine/CollisionHeightMap.h
@@ -1,19 +1,100 @@
+/**
+ * \file	CollisionHeightMap.h
+ *
+ * \brief	Declares the collision height map class.
+ */
+
 #pragma once
 #include "CollisionWorldSingleton.h"
 #include <BulletCollision/CollisionShapes/btHeightfieldTerrainShape.h>
 #include <vector>
 
+/**
+ * \class	CollisionHeightMap
+ *
+ * \brief	Collision height map.
+ *
+ * \author	Wise Bear
+ * \date	4/10/2013
+ */
+
 class CollisionHeightMap
 {
 public:
+
+	/**
+	 * \fn	CollisionHeightMap::CollisionHeightMap(float width, float length,
+	 * 		std::vector<float> HeightMapData, float MinHeight, float MaxHeight, float Normal);
+	 *
+	 * \brief	Constructor.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \param	width		 	The width.
+	 * \param	length		 	The length.
+	 * \param	HeightMapData	Information describing the height map.
+	 * \param	MinHeight	 	Height of the minimum.
+	 * \param	MaxHeight	 	Height of the maximum.
+	 * \param	Normal		 	The normal.
+	 */
+
 	CollisionHeightMap(float width, float length, std::vector<float> HeightMapData, float MinHeight, float MaxHeight, float Normal);
+
+	/**
+	 * \fn	CollisionHeightMap::~CollisionHeightMap()
+	 *
+	 * \brief	Destructor.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 */
+
 	~CollisionHeightMap(){};
+
+	/**
+	 * \fn	void CollisionHeightMap::Scale(float scaleX, float scaleY, float scaleZ);
+	 *
+	 * \brief	Scales the height map collision mesh.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \param	scaleX	The scale x coordinate.
+	 * \param	scaleY	The scale y coordinate.
+	 * \param	scaleZ	The scale z coordinate.
+	 */
+
 	void Scale(float scaleX, float scaleY, float scaleZ);
+
+	/**
+	 * \fn	void CollisionHeightMap::Translate(float translateX, float translateY, float translateZ);
+	 *
+	 * \brief	Translates the height map collision mesh.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \param	translateX	The translate x coordinate.
+	 * \param	translateY	The translate y coordinate.
+	 * \param	translateZ	The translate z coordinate.
+	 */
+
 	void Translate(float translateX, float translateY, float translateZ);
 
 
 protected:
 private:
+
+	/**
+	 * \brief	The height map.
+	 */
+
 	btRigidBody* HeightMap;
+
+	/**
+	 * \brief	The data.
+	 */
+
 	std::vector<float> data;
 };
--- GameEngine/GameEngine/CollisionWorldSingleton.h
+++ GameEngine/GameEngine/CollisionWorldSingleton.h
@@ -1,15 +1,51 @@
-/*Collision World Class, this class stores the collision world object and contains functions for manipulating it.*/
+/**
+ * \file	CollisionWorldSingleton.h
+ *
+ * \brief	Declares the collision world singleton class.
+ */
+
+
 #pragma once
 #include <btBulletDynamicsCommon.h>
 #ifndef COLLISION_WORLD_SINGLETON
+
+/**
+ * \def	COLLISION_WORLD_SINGLETON
+ *
+ * \brief	A macro that defines collision world singleton.
+ *
+ * \author	Wise Bear
+ * \date	4/10/2013
+ */
+
 #define COLLISION_WORLD_SINGLETON
+
+/**
+ * \class	CollisionWorldSingleton
+ *
+ * \brief	Collision world singleton.
+ *
+ * \author	Wise Bear
+ * \date	4/10/2013
+ */
 
 class CollisionWorldSingleton
 {
 private:
-	
+
+	/**
+	 * \fn	CollisionWorldSingleton::CollisionWorldSingleton()
+	 *
+	 * \brief	Default constructor.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 */
+
 	CollisionWorldSingleton()
 	{
+		 
+
 		 btBroadphaseInterface* broadphase = new btDbvtBroadphase();
 		 btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
 		 btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
@@ -18,16 +54,82 @@ private:
 		 DynamicsWorld->setGravity(btVector3(0,0,0));
 		 stepped = false;
 	}
-	
+
+	/**
+	 * \brief	true if the collision world has been stepped at all.
+	 */
+
 	bool stepped;
+
+	/**
+	 * \brief	The dynamics world all collision occurs in here.
+	 */
+
 	btDiscreteDynamicsWorld* DynamicsWorld;
 public:
 
+	/**
+	 * \fn	bool CollisionWorldSingleton::Stepped();
+	 *
+	 * \brief	returns the stepped boolean.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \return	true if it succeeds, false if it fails.
+	 */
+
 	bool Stepped();
+
+	/**
+	 * \fn	void CollisionWorldSingleton::AddRigidBody(btRigidBody* RigidBody);
+	 *
+	 * \brief	Adds a rigid body to the collision world.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \param [in,out]	RigidBody	If non-null, the rigid body.
+	 */
+
 	void AddRigidBody(btRigidBody* RigidBody);
+
+	/**
+	 * \fn	void CollisionWorldSingleton::StepWorld();
+	 *
+	 * \brief	Steps the collision world world.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 */
+
 	void StepWorld();
+
+	/**
+	 * \fn	void CollisionWorldSingleton::SetGravity(float y);
+	 *
+	 * \brief	Sets a gravity.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \param	y	The y coordinate.
+	 */
+
 	void SetGravity(float y);
 	//~CollisionWorldSingleton(){};
+
+	/**
+	 * \fn	static CollisionWorldSingleton* CollisionWorldSingleton::Instance();
+	 *
+	 * \brief	Gets the instance.
+	 *
+	 * \author	Wise Bear
+	 * \date	4/10/2013
+	 *
+	 * \return	null if it fails, else.
+	 */
+
 	static CollisionWorldSingleton* Instance();
 };
 
