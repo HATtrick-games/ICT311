c2b1c129ea44b05e02be062c8e59b540d66465fd
Arran <arran_ford@live.com>
2013-05-19 22:14:09 +0800

    added some functions to game object

--- Sanguine Wanderer/Sanguine Wanderer/GameObject.cpp
+++ Sanguine Wanderer/Sanguine Wanderer/GameObject.cpp
@@ -12,3 +12,12 @@ glm::vec3 GameObject::GetPosition()
 	return Position;
 }
 
+void GameObject::InitialiseCollisionBody(glm::vec3 Pos)
+{
+	cbCollisionObject = new CollisionBody(Pos);
+}
+
+void GameObject::SetVelocity(glm::vec2 velocitypass)
+{
+	cbCollisionObject->SetVelocityGravity(velocitypass.x,velocitypass.y);
+}
--- Sanguine Wanderer/Sanguine Wanderer/GameObject.h
+++ Sanguine Wanderer/Sanguine Wanderer/GameObject.h
@@ -10,9 +10,12 @@ public:
 	GameObject();
 	glm::vec3 GetPosition();
 	glm::vec3 GetVelocity(); 
+	void SetVelocity(glm::vec2 Velocity);
+	void InitialiseCollisionBody(glm::vec3 Pos);
 	virtual void Update() = 0;
 
 protected:
+	
 	CollisionBody* cbCollisionObject;
 	glm::vec3 Position;
 	glm::vec3 Velocity;
