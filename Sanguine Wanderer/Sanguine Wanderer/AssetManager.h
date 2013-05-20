c02dfef385f84952bbddfed8b74c25e36dfc58bd
Arran <arran_ford@live.com>
2013-05-20 22:55:07 +0800

    Asset factor and manager added

---
+++ Sanguine Wanderer/Sanguine Wanderer/AssetManager.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include "IAsset.h"
+#include "GameAssetFactory.h"
+
+#include <boost/smart_ptr.hpp>
+
+using namespace std;
+
+/**
+ * \class	AssetManager
+ *
+ * \brief	Manager for assets.
+ */
+
+class AssetManager
+{
+public:
+
+	/**
+	 * \fn	AssetManager::AssetManager(void);
+	 *
+	 * \brief	Default constructor.
+	 */
+	AssetManager(void);
+
+	/**
+	 * \fn	AssetManager::~AssetManager(void);
+	 *
+	 * \brief	Destructor.
+	 */
+	~AssetManager(void);
+
+	/**
+	 * \fn	bool AssetManager::AddAsset(string filename, string type);
+	 *
+	 * \brief	Adds an asset to 'type'.
+	 *
+	 * \param	filename	Filename of the file.
+	 * \param	type		The type.
+	 *
+	 * \return	true if it succeeds, false if it fails.
+	 */
+	bool AddAsset(string filename, string type);
+
+	/**
+	 * \fn	IAsset* AssetManager::GetAsset(string filename);
+	 *
+	 * \brief	Gets an asset.
+	 *
+	 * \param	filename	Filename of the file.
+	 *
+	 * \return	null if it fails, else the asset.
+	 */
+	IAsset* GetAsset(string filename);
+
+	/**
+	 * \fn	bool AssetManager::RemoveAsset(string filename);
+	 *
+	 * \brief	Removes the asset described by filename.
+	 *
+	 * \param	filename	Filename of the file.
+	 *
+	 * \return	true if it succeeds, false if it fails.
+	 */
+	void RemoveAsset(string filename);
+
+	/**
+	 * \fn	bool AssetManager::RemoveAllAssets();
+	 *
+	 * \brief	Removes all assets.
+	 *
+	 * \return	true if it succeeds, false if it fails.
+	 */
+	bool RemoveAllAssets();
+
+	static boost::scoped_ptr<AssetManager>* GetInstance();
+private:
+	map<string, IAsset*> mGameAssets;   //!< The game assets
+
+	static boost::scoped_ptr<AssetManager> pAssetManager;
+};
+
