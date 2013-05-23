735b30c32e402db02a76681592d90ef52f5889e9
Hamish <chaokel.mail@gmail.com>
2013-05-23 09:56:49 +0800

    sync

--- Sanguine Wanderer/Sanguine Wanderer/Game.cpp
+++ Sanguine Wanderer/Sanguine Wanderer/Game.cpp
@@ -123,7 +123,7 @@ void Game::Init()
 	(Knight->GetMesh())->SetFile("./data/KnightDefault.obj");
 	(Knight->GetMesh())->Load();
 
-	Knight->SetPosition(glm::vec3(0,0,-10));
+	Knight->SetPosition(glm::vec3(5,0,-10));
 	Knight->SetScale(glm::vec3(0.1,0.1,0.1));
 	Knight->SetRotation(glm::vec3(0,0,0));
 	Knight->InitialiseCollisionBody();
