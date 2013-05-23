07ce80ec52b86c70f2a9ac05d1c4ae49ab063fbf
Arran <arran_ford@live.com>
2013-05-23 12:23:54 +0800

    derp

--- Sanguine Wanderer/Sanguine Wanderer/Game.cpp
+++ Sanguine Wanderer/Sanguine Wanderer/Game.cpp
@@ -9,7 +9,7 @@ Game::Game()
 	Player = new PlayerObject(glm::vec3(0,10,-100),glm::vec3(0,10,-19),glm::vec3(0,1,0));
 	
 	plane = new GroundObject();
-	plane->InitialiseCollisionBody(glm::vec3(0,3,0), 10000, 1,10000);
+	plane->InitialiseCollisionBody(glm::vec3(0,1,0), 10000, 1,10000);
 	plane1 = new GroundObject();
 	plane1->InitialiseCollisionBody(glm::vec3(0,0,320), 100000,100000,1);
 	
@@ -224,6 +224,7 @@ void Game::InitialiseProps()
 	Temp->SetMesh(new Mesh);
 	Temp->GetMesh()->SetFile("./data/housebest.obj");
 	Temp->GetMesh()->Load();
+
 	Temp->SetPosition(glm::vec3(10,1,-60));
 	Temp->SetScale(glm::vec3(0.15,0.15,0.15));
 	Temp->SetRotation(glm::vec3(0,0,0));
@@ -261,6 +262,7 @@ void Game::InitialiseProps()
 	Temp->GetMesh()->SetFile("./data/RockObj.obj");
 	Temp->GetMesh()->Load();
 	Temp->SetPosition(glm::vec3(-19,0,-80));
+
 	Temp->SetScale(glm::vec3(0.15,0.15,0.15));
 	Temp->SetRotation(glm::vec3(0,0,0));
 	Temp->InitialiseCollisionBody();
