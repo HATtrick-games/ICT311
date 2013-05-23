ce6a7eed9642da1ddbbed9ed6b40805871053e5b
Arran <arran_ford@live.com>
2013-05-23 19:54:38 +0800

    aaa

--- Sanguine Wanderer/Sanguine Wanderer/PropObject.h
+++ Sanguine Wanderer/Sanguine Wanderer/PropObject.h
@@ -1,7 +1,26 @@
+/**
+ * \file	PropObject.h
+ *
+ * \brief	Declares the property object class.
+ */
+
 #include "GameObject.h"
+
+/**
+ * \class	PropObject
+ *
+ * \brief	Property object.
+ *
+ * \author	Arran Ford
+ * \date	5/23/2013
+ */
+
 class PropObject : public GameObject
 {
 public:
+	/**
+	*\brief default constructer
+	*/
 	PropObject();
 private:
 protected:
